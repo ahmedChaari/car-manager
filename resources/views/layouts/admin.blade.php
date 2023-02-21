99050798c00cc0f5eae01ad5e1454212281beb03
ahmedChaari <ahmed17ch@gmail.com>
2023-02-21 17:29:27 +0100

    get name app

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>{{ config('app.name') }}</title>
     <style>
         .swal-button--confirm {
             background-color: #a52b06 !important;
